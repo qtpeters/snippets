97b9a0dea2e401ac2f3d428a78af302a44fe00b2
qtpeter <quinn.peters@nissint.com>
2021-01-26 06:49:21 -0500

    Added remove-service script to systemd folder

---
+++ systemd/remove-service.sh
@@ -0,0 +1,8 @@
+systemctl stop [servicename]
+systemctl disable [servicename]
+rm /etc/systemd/system/[servicename]
+rm /etc/systemd/system/[servicename] # and symlinks that might be related
+rm /usr/lib/systemd/system/[servicename] 
+rm /usr/lib/systemd/system/[servicename] # and symlinks that might be related
+systemctl daemon-reload
+systemctl reset-failed
